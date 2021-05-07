f5ca2a8a1b9dfc8b1d375b350409a8839bc01b10
yekyuhwang <4leaf.hyk@gmail.com>
2021-05-07 15:54:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# LoL Board Web Project For Education
+
+<pre>
+     Node Express Web Server And View engine Pug With DB thath Mysql
+</pre>
+
+## SCREENS
+
+- [ ] Home Screen ("/")
+- [ ] Board List Screen ("/board/list")
+- [ ] Board write Screen ("/board/write")
+- [ ] Board Detail Screen ("/board/detail?id=${id}")
+
+## LOGICS
+
+- [ ] SELECT BOARD
+- [ ] SELECT EMP
+- [ ] INSERT INTO BOARD
+- [ ] UPDATE BOARD
+- [ ] DELETE BOARD
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import path from "path";
+import bodyParser from "body-parser";
+
+// body-parser -> 미들웨어이고, 미들웨어는 핸들러(컨트롤러)로 들어가기전에 꼭 body-parser에 들렸다가 간다는 뜻이다.
+// body-parser -> post 방식으로 암호화가 되어 전송할때 필요
+
+const app = express();
+
+const PORT = 7000;
+
+app.set("view engine", "pug");
+app.use(helmet());
+app.use(morgan(`dev`));
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//  제이슨 형태에서 암호화가 되어 온다.
+
+app.listen(PORT, () => {
+  console.log(`🍀 LOL_BOARD WEB PROJECT - http://localhost:${PORT}`);
+});
